437e0d7644b9e2b4b66ea5e9e20a7d912e8e8810
Jacob Fedrigon <jacobfed@umich.edu>
2025-02-09 22:06:17 -0500

    most keymaps

--- init.lua
+++ init.lua
@@ -156,6 +156,30 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+vim.opt.nu = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.g.netrw_bufsettings = "noma,nomod,nu,nowrap,ro,nobl"
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+
+vim.g.mapleader = " "
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
@@ -924,6 +948,7 @@ require("lazy").setup({
 		-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
 		opts = {
 			ensure_installed = {
+				"c_sharp",
 				"bash",
 				"c",
 				"diff",
