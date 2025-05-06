95850c99b83a97233bb295cd45b30d9ef12beba8
Jacob Fedrigon <jfedrigon@hagerty.com>
2025-05-05 22:31:07 -0400

    Add new keybinding

--- init.lua
+++ init.lua
@@ -118,6 +118,8 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 -- Diagnostic keymaps
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
+vim.keymap.set("n", "<leader>bd", ":bp|bd #<CR>", { noremap = true })
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
